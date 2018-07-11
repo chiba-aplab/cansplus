00d48dcf875922b4d34f1f25e8250738e997ff13
Yosuke MATSUMOTO <ymatumot@chiba-u.jp>
2018-07-12 00:21:29 +0900

    コンパイルオプションでflatMPIをデフォルトに。その他前回のコミットし忘れ。

--- 3d/md_kh/integrate.f90
+++ 3d/md_kh/integrate.f90
@@ -69,10 +69,6 @@ contains
   call convert__ptoc(ix,jx,kx,gm,ro,pr,vx,vy,vz,bx,by,bz &
                     ,rx,ry,rz,ee)
 
-  curx=0.d0  
-  cury=0.d0  
-  curz=0.d0
-
   ro1=ro
   rx1=rx
   ry1=ry
@@ -111,12 +107,12 @@ contains
   call flux_calc__hlld(row,prw,vxw,vyw,vzw,bx_m,byw,bzw,gm,margin,ix,jx,kx &
        ,frox,feex,frxx,fryx,frzx,fbyx,fbzx)
 
-  call flux_calc__fbres(mdir,margin,ix,jx,kx,fbyx,curz,eta,-1.0d0 &
-       ,fbyxr)
-  call flux_calc__fbres(mdir,margin,ix,jx,kx,fbzx,cury,eta,+1.0d0 &
-       ,fbzxr)
-  call flux_calc__feres(mdir,margin,ix,jx,kx,feex,curx,cury,curz,bx,by,bz,eta &
-       ,feexr)
+!  call flux_calc__fbres(mdir,margin,ix,jx,kx,fbyx,curz,eta,-1.0d0 &
+!       ,fbyxr)
+!  call flux_calc__fbres(mdir,margin,ix,jx,kx,fbzx,cury,eta,+1.0d0 &
+!       ,fbzxr)
+!  call flux_calc__feres(mdir,margin,ix,jx,kx,feex,curx,cury,curz,bx,by,bz,eta &
+!       ,feexr)
 !-----Step 1b.---------------------------------------------------------|
 ! compute flux at y-direction
 ! set L/R state at y-direction
@@ -141,21 +137,21 @@ contains
   call flux_calc__hlld(row,prw,vyw,vzw,vxw,by_m,bzw,bxw,gm,margin,ix,jx,kx &
        ,froy,feey,fryy,frzy,frxy,fbzy,fbxy)
 
-  call flux_calc__fbres(mdir,margin,ix,jx,kx,fbzy,curx,eta,-1.0d0 &
-       ,fbzyr)
-  call flux_calc__fbres(mdir,margin,ix,jx,kx,fbxy,curz,eta,+1.0d0 &
-       ,fbxyr)
-  call flux_calc__feres(mdir,margin,ix,jx,kx,feey,curx,cury,curz,bx,by,bz,eta &
-       ,feeyr)
+!  call flux_calc__fbres(mdir,margin,ix,jx,kx,fbzy,curx,eta,-1.0d0 &
+!       ,fbzyr)
+!  call flux_calc__fbres(mdir,margin,ix,jx,kx,fbxy,curz,eta,+1.0d0 &
+!       ,fbxyr)
+!  call flux_calc__feres(mdir,margin,ix,jx,kx,feey,curx,cury,curz,bx,by,bz,eta &
+!       ,feeyr)
 !-----Step 1c.---------------------------------------------------------|
 ! compute flux at z-direction
 ! set L/R state at z-direction
 !
   mdir = 3
   
-!  call lr_state__MP5(mdir,ix,jx,kx,ro,pr &
-!       ,vz,vx,vy,bz,bx,by,phi &
-!       ,ch,gm,row,prw,vzw,vxw,vyw,bzw,bxw,byw,phiw,ccx,ccy,ccz)
+  call lr_state__MP5(mdir,ix,jx,kx,ro,pr &
+       ,vz,vx,vy,bz,bx,by,phi &
+       ,ch,gm,row,prw,vzw,vxw,vyw,bzw,bxw,byw,phiw,ccx,ccy,ccz)
 !  call lr_state__MSCL2(mdir,ix,jx,kx,ro,pr &
 !       ,vz,vx,vy,bz,bx,by,phi &
 !       ,ch,gm,row,prw,vzw,vxw,vyw,bzw,bxw,byw,phiw,dx,dy,dz)
@@ -163,13 +159,13 @@ contains
 !       ,ro,pr,vx,vy,vz,bx,by,bz,phi &
 !       ,row,prw,vxw,vyw,vzw,bxw,byw,bzw,phiw)
 
-!  call flux_calc__bp(ix,jx,kx,bzw,phiw &
-!       ,bz_m,phi_m,ch)
+  call flux_calc__bp(ix,jx,kx,bzw,phiw &
+       ,bz_m,phi_m,ch)
 
-!  call flux_calc__glm(bz_m,phi_m,ch,fbzz,fphiz,ix,jx,kx)
+  call flux_calc__glm(bz_m,phi_m,ch,fbzz,fphiz,ix,jx,kx)
 
-!  call flux_calc__hlld(row,prw,vzw,vxw,vyw,bz_m,bxw,byw,gm,margin,ix,jx,kx &
-!       ,froz,feez,frzz,frxz,fryz,fbxz,fbyz)
+  call flux_calc__hlld(row,prw,vzw,vxw,vyw,bz_m,bxw,byw,gm,margin,ix,jx,kx &
+       ,froz,feez,frzz,frxz,fryz,fbxz,fbyz)
 
 !  call flux_calc__fbres(mdir,margin,ix,jx,kx,fbxz,cury,eta,-1.0d0 &
 !       ,fbxzr)
@@ -192,40 +188,40 @@ contains
 
            ro(i,j,k) = k1*ro1(i,j,k)+k2*(+ro(i,j,k) &
                 +dtodx*(frox(i-1,j,k)-frox(i,j,k))  &
-                +dtody*(froy(i,j-1,k)-froy(i,j,k)) )! &
-!                +dtodz*(froz(i,j,k-1)-froz(i,j,k)))
+                +dtody*(froy(i,j-1,k)-froy(i,j,k)) )&
+                +dtodz*(froz(i,j,k-1)-froz(i,j,k)))
            ee(i,j,k) = k1*ee1(i,j,k)+k2*(+ee(i,j,k)  &
                 +dtodx*(feexr(i-1,j,k)-feexr(i,j,k)) &
-                +dtody*(feeyr(i,j-1,k)-feeyr(i,j,k)))! &
-!                +dtodz*(feezr(i,j,k-1)-feezr(i,j,k)))
+                +dtody*(feeyr(i,j-1,k)-feeyr(i,j,k)))&
+                +dtodz*(feezr(i,j,k-1)-feezr(i,j,k)))
            rx(i,j,k) = k1*rx1(i,j,k)+k2*(+rx(i,j,k) &
                 +dtodx*(frxx(i-1,j,k)-frxx(i,j,k))  &
-                +dtody*(frxy(i,j-1,k)-frxy(i,j,k)) )! &
-!                +dtodz*(frxz(i,j,k-1)-frxz(i,j,k)) )
+                +dtody*(frxy(i,j-1,k)-frxy(i,j,k)) )&
+                +dtodz*(frxz(i,j,k-1)-frxz(i,j,k)) )
            ry(i,j,k) = k1*ry1(i,j,k)+k2*(+ry(i,j,k) &
                 +dtodx*(fryx(i-1,j,k)-fryx(i,j,k))  &
-                +dtody*(fryy(i,j-1,k)-fryy(i,j,k)) )! &
-!                +dtodz*(fryz(i,j,k-1)-fryz(i,j,k)) )
+                +dtody*(fryy(i,j-1,k)-fryy(i,j,k)) )&
+                +dtodz*(fryz(i,j,k-1)-fryz(i,j,k)) )
            rz(i,j,k) = k1*rz1(i,j,k)+k2*(+rz(i,j,k) &
                 +dtodx*(frzx(i-1,j,k)-frzx(i,j,k))  &
-                +dtody*(frzy(i,j-1,k)-frzy(i,j,k)) )! &
-!                +dtodz*(frzz(i,j,k-1)-frzz(i,j,k)) )
+                +dtody*(frzy(i,j-1,k)-frzy(i,j,k)) )&
+                +dtodz*(frzz(i,j,k-1)-frzz(i,j,k)) )
            bx(i,j,k) = k1*bx1(i,j,k)+k2*(+bx(i,j,k)  &
                 +dtodx*(fbxx(i-1,j,k)-fbxx(i,j,k))   &
-                +dtody*(fbxyr(i,j-1,k)-fbxyr(i,j,k)) )!&
-!                +dtodz*(fbxzr(i,j,k-1)-fbxzr(i,j,k)) )
+                +dtody*(fbxyr(i,j-1,k)-fbxyr(i,j,k)) &
+                +dtodz*(fbxzr(i,j,k-1)-fbxzr(i,j,k)) )
            by(i,j,k) = k1*by1(i,j,k)+k2*(+by(i,j,k)  &
                 +dtodx*(fbyxr(i-1,j,k)-fbyxr(i,j,k)) &
-                +dtody*(fbyy(i,j-1,k)-fbyy(i,j,k))  )! &
-!                +dtodz*(fbyzr(i,j,k-1)-fbyzr(i,j,k)) )
+                +dtody*(fbyy(i,j-1,k)-fbyy(i,j,k))   &
+                +dtodz*(fbyzr(i,j,k-1)-fbyzr(i,j,k)) )
            bz(i,j,k) = k1*bz1(i,j,k)+k2*(+bz(i,j,k)  &
                 +dtodx*(fbzxr(i-1,j,k)-fbzxr(i,j,k)) &
-                +dtody*(fbzyr(i,j-1,k)-fbzyr(i,j,k)) )!&
-!                +dtodz*(fbzz(i,j,k-1)-fbzz(i,j,k)) )
+                +dtody*(fbzyr(i,j-1,k)-fbzyr(i,j,k)) &
+                +dtodz*(fbzz(i,j,k-1)-fbzz(i,j,k)) )
            phi(i,j,k) = k1*phi1(i,j,k)+k2*(+phi(i,j,k) &
                 +dtodx*(fphix(i-1,j,k)-fphix(i,j,k))   &
                 +dtody*(fphiy(i,j-1,k)-fphiy(i,j,k))   &
-!                +dtodz*(fphiz(i,j,k-1)-fphiz(i,j,k))   &
+                +dtodz*(fphiz(i,j,k-1)-fphiz(i,j,k))   &
                 )*exp(-dt*ch**2/cp**2)
 
         enddo
